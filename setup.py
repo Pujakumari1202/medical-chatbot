b2ac20903db149608c8cc04494c59f894ed243aa
Puja Kumari <149191257+Pujakumari1202@users.noreply.github.com>
2024-10-17 11:22:32 +0530

    updated files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='Medical Chatbot',
+    version='0.0.0',
+    author='Puja Kumari',
+    author_email='puja02538@gmail.com',
+    packages=find_packages(),
+    install_reuires=[]
+)
